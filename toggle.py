bdd3fa1408ba7a8b36e47de389de48e4b5a6050b
LUO Yan <lzl257@126.com>
2019-11-15 09:52:38 +0100

    Add hide_toggle2 function.

--- toggle.py
+++ toggle.py
@@ -35,3 +35,38 @@ def hide_toggle(for_next=False, hiding_text='Toggle show/hide'):
     )
 
     return HTML(html)
+
+def hide_toggle2(for_next=False, hiding_text='Toggle show/hide'):
+    this_cell = """$('div.cell.code_cell.rendered.selected')"""
+    next_cell = this_cell + '.next()'
+
+    toggle_text = hiding_text  # text shown on toggle link
+    target_cell = this_cell  # target cell to control with toggle
+    js_hide_current = ''  # bit of JS to permanently hide code in current cell (only when toggling next cell)
+
+    if for_next:
+        target_cell = next_cell
+        toggle_text += ' next cell'
+        js_hide_current = this_cell + '.find("div.input").hide();'
+
+    js_f_name = 'code_toggle_{}'.format(str(random.randint(1,2**64)))
+    js_f_name2 = 'code_toggle_{}'.format(str(random.randint(1,3**70)))
+
+    html = """
+        <script>
+            function {f_name}() {{
+                {cell_selector}.find('div.input').toggle();
+                {cell_selector}.find('div.output').toggle()
+            }}           
+
+            {js_hide_current};
+            {f_name}()
+        </script>
+    """.format(
+        f_name=js_f_name,
+        cell_selector=target_cell,
+        js_hide_current=js_hide_current, 
+        toggle_text=toggle_text
+    )
+
+    return HTML(html)
