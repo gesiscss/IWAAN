4c8ebc5914b4e692d4bf22a81bb41cda19005a6c
suhasini-gesis <suhasini.venkatesh@gesis.org>
2021-02-26 11:02:07 +0100

    Skipping the execution of the practice cells

---
+++ skip_kernel_extension.py
@@ -0,0 +1,19 @@
+from IPython import get_ipython
+
+def skip(line, cell=None):
+    '''Skips execution of the current line/cell if line evaluates to True.'''
+    if eval(line):
+        return
+
+    get_ipython().ex(cell)
+
+def load_ipython_extension(shell):
+    '''Registers the skip magic when the extension loads.'''
+    shell.register_magic_function(skip, 'line_cell')
+
+def unload_ipython_extension(shell):
+    '''Unregisters the skip magic when the extension unloads.'''
+    del shell.magics_manager.magics['cell']['skip']
+
+#loading custom extension
+load_ipython_extension(get_ipython())
